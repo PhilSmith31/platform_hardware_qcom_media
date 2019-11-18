5593837fb07977685aef11a01966231ec1cb6972
Philip Koslowski <philip.koslowski@gmail.com>
2019-11-18 23:12:07 +0100

    adjusted Android.mk for no platform subforlders

--- Android.mk
+++ Android.mk
@@ -1,13 +1,7 @@
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(TARGET_BOARD_AUTO),true)
 
-  QCOM_MEDIA_ROOT := $(call my-dir)/msm8974
-  ifneq ($(filter msm8996, $(TARGET_BOARD_PLATFORM)),)
-    QCOM_MEDIA_ROOT := $(call my-dir)/msm8996
-  endif
-  ifneq ($(filter msm8998, $(TARGET_BOARD_PLATFORM)),)
-    QCOM_MEDIA_ROOT := $(call my-dir)/msm8998
-  endif
+  QCOM_MEDIA_ROOT := $(call my-dir)
 
   ifneq ($(filter msm8610 msm8226 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
